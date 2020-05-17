fe5968155b3cf83531d1fe7034ebf3ba3b72fd83
Frank Chan <frankchan02@gmail.com>
2020-05-17 23:07:30 +0800

    Update general_functions.js

--- restaurantFYP/static/app/scripts/general_functions.js
+++ restaurantFYP/static/app/scripts/general_functions.js
@@ -14,3 +14,21 @@ function changeModalQuantity(id, i, price) {
 	}
 	else qty.setAttribute("class", "text-danger text-center");
 }
+
+function check_bill(token) {
+    const g_interval = setInterval(function () {
+        $.ajax({
+            type: 'POST',
+            url: '/force_logout',
+            data: {
+                csrfmiddlewaretoken: token,
+            },
+            success: function (data) {
+                if (data == 'false') {
+                    window.location.href = '/logout';
+                }
+            }
+        });
+    }, 5000);
+
+}
